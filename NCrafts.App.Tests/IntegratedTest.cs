5030d6cce1fd0b55adc94c980a4fb7732198708b
anthyme <anthyme@live.com>
2015-12-28 17:41:37 +0100

    Add IntegratedTest Add SessionDetailsViewModelTest

---
+++ NCrafts.App.Tests/IntegratedTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Unity;
+using NCrafts.App.Common.Infrastructure;
+
+namespace NCrafts.App.Tests
+{
+    public class IntegratedTest
+    {
+        private static readonly IUnityContainer ParentContainer = DependencyConfigurator.Configure();
+        protected IUnityContainer Container;
+
+        public IntegratedTest()
+        {
+            Container = ParentContainer.CreateChildContainer();
+        }
+    }
+}
--- NCrafts.App.Tests/Sessions/SessionDetailsViewModelTest.cs
+++ NCrafts.App.Tests/Sessions/SessionDetailsViewModelTest.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections.Generic;
 using Microsoft.Practices.Unity;
 using NCrafts.App.Core.Common;
-using NCrafts.App.Core.Sessions.Query;
 using NCrafts.App.Sessions;
 using Shouldly;
 using Xunit;
 
 namespace NCrafts.App.Tests.Sessions
 {
-    public class Given_a_SessionsViewModel : IntegratedTest
+    public class Given_a_SessionDetailsViewModel : IntegratedTest
     {
         [Fact]
         public async void When_starting_Then_sessions_are_loaded()
         {
-            var sut = Container.Resolve<SessionsViewModel>();
+            var sut = Container.Resolve<SessionDetailsViewModel>();
+            sut.Init(new SessionId("1"));
 
             await sut.Start();
 
-            sut.Sessions.ShouldNotBeEmpty();
+            sut.Session.ShouldNotBeNull();
+            sut.Session.Id.ShouldBe(new SessionId("1"));
         }
     }
 }
